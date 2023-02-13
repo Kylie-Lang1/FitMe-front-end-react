8846eebcdc102d1c1b46756cb4d977eaeb0c2d20
Kylie-Lang1 <kylielang@pursuit.org>
2023-02-13 11:39:27 -0500

    hold feat

--- src/pages/ClosetIndex.js
+++ src/pages/ClosetIndex.js
@@ -4,8 +4,8 @@ function ClosetIndex() {
     return (
         <div>
             <h1 className='text-center py-10'>My Closet</h1>
-            <h2>Favorites</h2>
-                <ClothingCards isFavorite={true}/>
+            {/* <h2>Favorites</h2>
+                <ClothingCards isFavorite={true}/> */}
             <h2>Clothes</h2>
                 <ClothingCards isFavorite={false}/>
         </div>
