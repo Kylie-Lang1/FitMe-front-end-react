e195843df3b16f695f73553e8f5d2e967458e7ee
Kylie-Lang1 <kylielang@pursuit.org>
2023-02-13 11:35:52 -0500

    fix select/unselected style change handler

--- src/components/ClothingCard.js
+++ src/components/ClothingCard.js
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function ClothingCard({clothes, id, createOutfit}) {
+function ClothingCard({clothes, id, createOutfit, isSelected, setIsSelected}) {
+    const unselected = "border-none"
+    const selected = "border-2 border-gray-500 drop-shadow-lg"
+    
     const [thisItem, setThisItem] = useState({});
+    const [style, setStyle] = useState(unselected);
 
     const handleSelect = () => {
         const copyClothes = clothes
         copyClothes.is_selected = !copyClothes.is_selected
+
+        style === unselected ? setStyle(selected) : setStyle(unselected)
 
         // const newClothes = {is_selected: !clothes.is_selected, ...clothes}
 
@@ -25,9 +31,9 @@ function ClothingCard({clothes, id, createOutfit}) {
                 <img 
                     src={clothes.img_url} 
                     alt={clothes.name}
-                    className={'w-64 h-64 object-cover ' + `${thisItem.is_selected ? "border-2 border-gray-500 drop-shadow-lg" : ""}`}
+                    className={'w-64 h-64 object-cover ' + `${style}`}
                 />
-                <h1 className="w-64 font-bold truncate">{clothes.name}</h1>
+                <h1 className="w-64 font-bold truncate">{clothes.name}</h1> 
                 <p>{clothes.brand}</p>
             </div>
         ) : (
